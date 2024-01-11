7f7c724a9963d8239de780f6d63752a570dac83e
Abdulhamid Patel <114407664+Abdulhamid109@users.noreply.github.com>
2024-01-12 00:46:46 +0530

    Add files via upload

---
+++ lib/pages/FeedMain.dart
@@ -0,0 +1,201 @@
+import 'package:digital_ration/pages/Dasboard.dart';
+import 'package:digital_ration/pages/complaint_page.dart';
+import 'package:digital_ration/pages/feedback_page.dart';
+import 'package:flutter/material.dart';
+
+class FeedMain extends StatefulWidget {
+  const FeedMain({Key? key}) : super(key: key);
+
+  @override
+  State<FeedMain> createState() => _FeedMainState();
+}
+
+class _FeedMainState extends State<FeedMain> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    String item1 = "Feedback Page";
+    String item2 = "Complaint Page";
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 60,
+          backgroundColor: Colors.blueAccent,
+          elevation: 0,
+          title: const Column(
+            children: [
+              Text(
+                "Welcome",
+                style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                "Mr S.A.Patil",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              )
+            ],
+          ),
+          // centerTitle: true,
+          actions: [
+            const Text(
+              "FeedBack",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            const SizedBox(width: 20,),
+            const Icon(Icons.person_outline),
+            // IconButton(
+            //   icon: const Icon(Icons.arrow_drop_down_circle),
+            //   onPressed: () {
+            //     //Do Something
+            //   },
+            // ),
+            GestureDetector(
+            child: PopupMenuButton(
+              itemBuilder: (context)=>[
+                PopupMenuItem(
+                  value: item1,
+                  child: Text(item1),
+                  onTap: () =>  setState(() {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const feedback_page()));
+                  })
+                ),
+                PopupMenuItem(
+                  value: item2,
+                  child: Text(item2),
+                  onTap: () =>  setState(() {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const complaint_page()));
+                  })
+                ),
+                
+              ],
+              icon: const Icon(Icons.arrow_drop_down_circle),
+              ),
+          )
+        
+          ],
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              // Do Something
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Stack(
+              // clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: [
+                Card(
+                  elevation: 8,
+                  child: Container(
+                    height: height / 1.5,
+                    width: width / 1.3,
+                    color: Colors.white30,
+                  ),
+                ),
+                //Avatar container
+                Container(
+                  margin: const EdgeInsets.only(bottom: 485),
+                  child: const CircleAvatar(
+                    backgroundColor: Colors.black54,
+                    radius: 50,
+                    backgroundImage: AssetImage("images/user.jpg"),
+                  ),
+                ),
+                //username container
+                Container(
+                  margin: const EdgeInsets.only(bottom: 335),
+                  child: const Text(
+                    "USERNAME",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                // Full name Container
+                Container(
+                  margin: const EdgeInsets.only(bottom: 250, right: 80),
+                  child: const Text(
+                    "Full Name: ABC USER",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                // Adhar no container
+                Container(
+                  margin: const EdgeInsets.only(bottom: 190, right: 15),
+                  child: const Text(
+                    "Adhar No : XXXXXXXXXXX124",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 190),
+                  child: Card(
+                    elevation: 5,
+                    child: Container(
+                      clipBehavior: Clip.none,
+                      height: height / 2.7,
+                      width: width / 1.5,
+                      decoration: const BoxDecoration(
+                        color: Colors.black38,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Feedback No: 101",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              "Great Work!!",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w300),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Container(
+                    
+                    margin: const EdgeInsets.only(top: 580),
+                    child:  ElevatedButton(
+                      style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.blueAccent)),
+                      onPressed: () {
+                        // go to Dashboard
+                        setState(() {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Dashboard()));
+                        });
+                      },
+                      child: const Text("Dashboard",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.black),),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/feedback_page.dart
@@ -0,0 +1,179 @@
+import 'package:digital_ration/pages/FeedMain.dart';
+import 'package:digital_ration/pages/complaint_page.dart';
+import 'package:flutter/material.dart';
+
+class feedback_page extends StatefulWidget {
+  const feedback_page({Key? key}) : super(key: key);
+
+  @override
+  State<feedback_page> createState() => _feedback_pageState();
+}
+
+class _feedback_pageState extends State<feedback_page> {
+  @override
+  Widget build(BuildContext context) {
+    List names = [
+      "Abhinav Singh",
+      "Akhil Shrivastav",
+      "Samay Gurjar",
+      "Sheyansh seth",
+      "ABC User"
+    ];
+    String item1 = "Feedback Page";
+    String item2 = "Complaint Page";
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 60,
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
+        title: const Column(
+          children: [
+            Text(
+              "Welcome",
+              style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Text(
+              "Mr S.A.Patil",
+              style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            )
+          ],
+        ),
+        // centerTitle: true,
+        actions: [
+          const Text(
+            "FeedBack",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          const Icon(Icons.person_outline),
+          // IconButton(
+          //   icon: const Icon(Icons.arrow_drop_down_circle),
+          //   onPressed: () {
+          //     //Do Something
+          //   },
+          // ),
+          GestureDetector(
+            child: PopupMenuButton(
+              itemBuilder: (context)=>[
+                PopupMenuItem(
+                  value: item1,
+                  child: Text(item1),
+                  onTap: () =>  setState(() {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const feedback_page()));
+                  })
+                ),
+                PopupMenuItem(
+                  value: item2,
+                  child: Text(item2),
+                  onTap: () => setState(() {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const complaint_page()));
+                  })
+                ),
+                
+              ],
+              icon: const Icon(Icons.arrow_drop_down_circle),
+              ),
+          )
+        
+        ],
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            // Do Something
+          },
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: names.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Card(
+              elevation: 8,
+              child: Container(
+                width: 200,
+                height: 170,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.5),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Name: ${names[index]}",
+                        style: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      const Text(
+                        "Phone No: +91XXXXXXXX78",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      const Text(
+                        "R.Card No: XXXXXXXXXXXXX15",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      const Text(
+                        "Latest Collection date: 12-4-2023",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.blueAccent)),
+                              onPressed: () {
+                                // Feed(names[index]);
+                                setState(() {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const FeedMain()));
+                                });
+                              },
+                              child: const Text("Details",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),))
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
